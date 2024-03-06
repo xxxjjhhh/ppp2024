20acefb193b60d69b9178605e4dbdc24014a0e8e
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2024-03-07 01:08:37 +0900

    Create 2.py

---
+++ hw02/2.py
@@ -0,0 +1,11 @@
+# 키와 몸무게가 임의로 주어졌을 때 BMI를 구하시오
+# BMI = 몸무게 (kg) / 키 (m)^2
+# weight = 60, height = 170
+
+weight = 60
+height = 170
+
+mHeight = 170/100
+
+bmi = weight / mHeight*mHeight
+print(bmi)
