9fa3d862d75456bcb4181f8ab25571053084fe27
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2024-03-13 17:59:38 +0900

    Update sol05.py

--- hw03/sol05.py
+++ hw03/sol05.py
@@ -3,10 +3,12 @@
 # 딸기(설향) 34kcal/100g
 # 바나나 77kcal/100g
 
+#input
 fruit_hala = int(input("섭취한 한라봉의 양을 입력하세요. (g) : "))
 fruit_strawberry = int(input("섭취한 딸기(설향)의 양을 입력하세요. (g) : "))
 fruit_banana = int(input("섭취한 바나나의 양을 입력하세요. (g) : "))
 
+#total
 total_kcal = fruit_hala*50/100 + fruit_strawberry*34/100 + fruit_banana*77/100
 
 print(f"섭취한 양에 대한 총 칼로리는 {total_kcal}kcal 입니다.")
