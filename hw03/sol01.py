86178626f6018f3743fcff2308af07dd4043a09a
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2024-03-13 17:04:19 +0900

    Update sol01.py

--- hw03/sol01.py
+++ hw03/sol01.py
@@ -1,7 +1,7 @@
 # 섭씨를 화씨로 변환하는 프로그램을 작성하라.
-# 입력 : 30, 0
+# 입력 : 콘솔
 
-c_temp = 30
+c_temp = int(input("섭씨 온도를 입력하세요.(°C) : "))
 f_temp = ((c_temp * 9/5) + 32)
 
-print("섭씨 {}C는 화씨 {}F 입니다.".format(c_temp, f_temp))
+print("섭씨 {}°C는 화씨 {}°F 입니다.".format(c_temp, f_temp))
