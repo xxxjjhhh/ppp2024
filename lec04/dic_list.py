12e851c80d4a491f5a761faa318ceb28abffdb02
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2024-03-25 15:16:21 +0900

    Update dic_list.py

--- lec04/dic_list.py
+++ lec04/dic_list.py
@@ -1,7 +1,13 @@
+eat_fruits = ["딸기(설향)", "시과"]
+eat_grams = [50, 120]
 
-cal = {}
 
+cal = {"한라봉": 0.5, "딸기(설향)": 0.34, "바나나": 0.77}
 
-for fruit in cal:
-    if eat_fruit == fruit:
-        total_cal = cal[fruit] * eat_gram
+total_cal = 0
+idx = 0
+for eat_fruit in eat_fruits:
+    for fruit in cal:
+        if eat_fruit == fruit:
+            total_cal = cal[fruit] * eat_gram[idx]
+        idx = idx + 1
