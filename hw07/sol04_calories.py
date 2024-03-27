aeedfa0e0e58041a75f7e4c0dfc473cf17146aa2
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2024-03-27 21:55:49 +0900

    Update sol04_calories.py

--- hw07/sol04_calories.py
+++ hw07/sol04_calories.py
@@ -4,3 +4,26 @@
 # fruits={“딸기”: 300, “한라봉”: 150}, fruits_calorie_dic={"한라봉": 50, "딸기": 34, "바나나": 77}.
 # 과제 #06-01 활용. 함 수명은 total_calorie(fruits, fruits_calorie_dic)
 
+def total_calorie(fruits: dict, fruits_calorie_dic: dict) -> float:
+
+    #total
+    total_kcal = 0
+    for fruit in fruits:
+        total_kcal = total_kcal + fruits[fruit] * fruits_calorie_dic[fruit] / 100
+
+    return total_kcal
+
+def main() -> None:
+
+    #data
+    # 먹은 과일 data {"fruit" : 0}
+    fruits = {"딸기" : 300, "한라봉" : 150}
+    # 과일 100g kcal data {"furit" : kcal}
+    fruits_calorie_dic = {"한라봉" : 50, "딸기" : 34, "바나나" : 77}
+
+    #칼로리 계산
+    total_kcal = total_calorie(fruits, fruits_calorie_dic)
+    print(f"섭취한 양에 대한 총 칼로리는 {total_kcal}kcal 입니다.")
+
+if __name__ == "__main__":
+    main()
